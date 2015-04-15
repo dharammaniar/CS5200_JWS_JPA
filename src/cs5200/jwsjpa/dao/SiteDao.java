4e8f40809cd4d032fa5fc3dc036bc87e948803db
Dharam Maniar <maniar.d@husky.neu.edu>
2015-04-14 23:07:50 -0400

    Updated SiteDao for Q4

--- src/cs5200/jwsjpa/dao/SiteDao.java
+++ src/cs5200/jwsjpa/dao/SiteDao.java
@@ -6,23 +6,43 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.persistence.Query;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 import cs5200.jwsjpa.models.Site;
 
+@Path("/site")
 public class SiteDao {
 
 	EntityManagerFactory factory = Persistence.createEntityManagerFactory("CS5200_JWS_JPA");
 	EntityManager em = factory.createEntityManager();
 	
-	public Site findSite(int siteId) {
+	@GET
+	@Path("/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Site findSite(@PathParam("id") int siteId) {
 		return em.find(Site.class, siteId);
 	}
 	
+	@GET
+	@Path("/")
+	@Produces(MediaType.APPLICATION_JSON)
 	public List<Site> findAllSites() {
 		Query query = em.createQuery("select site from Site site");
 		return (List<Site>)query.getResultList();
 	}
 	
+	@PUT
+	@Path("/{id}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
 	public List<Site> updateSite(int siteId, Site site) {
 		em.getTransaction().begin();
 		site.setId(siteId);
@@ -31,7 +51,10 @@ public class SiteDao {
 		return findAllSites();
 	}
 	
-	public List<Site> removeSite(int siteId) {
+	@DELETE
+	@Path("/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Site> removeSite(@PathParam("id") int siteId) {
 		em.getTransaction().begin();
 		Site site = em.find(Site.class, siteId);
 		em.remove(site);
@@ -39,6 +62,10 @@ public class SiteDao {
 		return findAllSites();
 	}
 	
+	@POST
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
 	public List<Site> createSite(Site site) {
 		em.getTransaction().begin();
 		em.merge(site);
